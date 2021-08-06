b6f888fd159f63dbcaa8f7c7cd5fe286f056c4bf
Alvaro Cardozo <alvaro_car6@hotmail.com>
2021-08-06 14:52:56 -0500

    resolve base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/buy-my-funko/',
+  base: '/buy-my-funko-fe/',
   plugins: [vue()],
   resolve: {
     alias:{
