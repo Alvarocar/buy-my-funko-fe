e456720848d4a1412dc680a4a392bc1bbcd70e81
Valentine <dulzura64@gmail.com>
2021-08-15 20:29:56 -0500

    created card component

--- src/components/lists/ProductList.vue
+++ src/components/lists/ProductList.vue
@@ -1,37 +1,19 @@
 <template>
-    <div class="content" :key="product.id" v-for="product in products">
-      <article class="card">
-        <router-link to="/">
-          <img :src="product.image"/>
-          <figcaption>
-            <p class="title">{{product.title}}</p>
-            <span class="price">$ {{product.price}}</span>
-          </figcaption>
-        </router-link>
+    <div class="content">
+      <article :key="product.id" v-for="product in products">
+        <card-products :product="product"/>
       </article>
     </div>
 </template>
 <script>
+
 import { defineComponent, onMounted } from 'vue'
 import { mapActions, mapState, useStore } from 'vuex'
-
+import CardProducts from '../cards/CardProducts.vue'
 export default defineComponent({
+  components: { CardProducts },
    name:'ProductList',
-  setup(){
-    const {state} = useStore() 
-    console.log(state)
-    // /**
-    //  * @param {number} page
-    //  */
-    // const fetchProducts = (page)=>{
-    //   dispatch('product/getProducts', page)
-    // }
-    // onMounted(()=>{fetchProducts(0),
-    // console.log('mountedddd');
-    // }
-    // )
-  },
-  mounted(){
+       mounted(){
     this.getProducts(0)
   },
   methods:{
@@ -40,8 +22,8 @@ export default defineComponent({
     ])
   },
    computed: {
-     ...mapState('product',{
-       products:(state)=> state.product.products
+     ...mapState('product',{ 
+       products:(state)=> state.products
      })
    }
 })
@@ -55,32 +37,5 @@ export default defineComponent({
     column-gap: 1rem;
     row-gap: 1rem;
     padding: 1rem;
-  }
-
-  .card {
-    background-color: #fff;
-    padding: 0 0 2rem;
-    border-radius: 10px;
-    box-shadow: 2px 0px 10px #9E9E9E;
-    & img {
-      width: 100%;
-      height: auto;
-      border-radius: 10px 10px 0 0;
-    }
-    & a {
-      text-decoration: none;
-      color: #212121;
-      &:hover {
-        opacity: .8;
-      }
-    }
-    & .title {
-      opacity: .7;
-      font-size: .85rem;
-    }
-    & .price {
-      font-weight: bolder;
-      font-size: 1.2rem;
-    }
   }
 </style>
