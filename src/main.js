9171e1d3b5547c4ba327f07986808e0f51391697
Alvaro Cardozo <alvaro_car6@hotmail.com>
2021-08-17 20:59:45 -0500

    add element-plus config

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import Router from '@/router'
 import Store from '@/store'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
 createApp(App)
   .use(Router)
   .use(Store)
+  .use(ElementPlus)
   .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/buy-my-funko-fe/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [{
+        libraryName: 'element-plus',
+        esModule: true,
+        ensureStyleFile: true,
+        resolveStyle: (name) => {
+          name = name.slice(3)
+          return `element-plus/packages/theme-chalk/src/${name}.scss`;
+        },
+        resolveComponent: (name) => {
+          return `element-plus/lib/${name}`;
+        },
+      }]
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -20,5 +37,5 @@ export default defineConfig({
         `
       }
     }
-  }
+  },
 })
